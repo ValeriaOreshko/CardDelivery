1d4eceb55bfdcb1b8226d3936ae4a8fc1ed31cbc
Valeria Oreshko <oreshko-v@yandex.ru>
2023-08-04 16:11:23 +0300

    Fixed

--- src/test/java/ru/netology/CardDeliveryTest.java
+++ src/test/java/ru/netology/CardDeliveryTest.java
@@ -1,35 +1,40 @@
 package ru.netology;
 
+import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selectors.withText;
-import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.open;
+import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
+
+    private String generateDate(int addDays, String pattern) {
+        return LocalDate.now().plusDays(addDays).format(DateTimeFormatter.ofPattern(pattern));
+    }
 
     @Test
     void shouldTest() {
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Санкт-Петербург");
-
-        LocalDate currentDate = LocalDate.now();
-        LocalDate futureDate = currentDate.plusDays(3);
-        String dateText = futureDate.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
-
-        $("[data-test-id='date'] input").setValue("dateText");
+        String currentDate = generateDate(4, "dd.MM.yyyy");
+        $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
+        $("[data-test-id='date'] input").sendKeys(currentDate);
         $("[data-test-id='name'] input").setValue("Мария Иванова");
         $("[data-test-id='phone'] input").setValue("+79110000000");
         $("[data-test-id='agreement']").click();
         $(".button").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
     }
 
     @Test
@@ -40,21 +45,20 @@ public class CardDeliveryTest {
 
         $(".icon").click();
 
-        LocalDate currentDate = LocalDate.now();
-        LocalDate futureDate = currentDate.plusWeeks(1);
-        Long millis = futureDate.atStartOfDay(ZoneId.systemDefault()).toInstant().toEpochMilli();
-
-        while (!$(By.cssSelector("[data-day='" + millis + "']")).exists()) {
-            $(By.cssSelector("[data-step='1']")).click();
+        int dayToAdd = 7;
+        int defaultAddedDays = 3;
+        $("[data-test-id='date'] input").click();
+        if (!generateDate(defaultAddedDays, "MM").equals(generateDate(dayToAdd, "MM"))) {
+            $("[data-step='1']").click();
         }
-
-        $("[data-day='" + millis + "']").click();
-
+        $$(".calendar__day").findBy(text(generateDate(dayToAdd, "d"))).click();
         $("[data-test-id='name'] input").setValue("Иван Петров");
         $("[data-test-id='phone'] input").setValue("+79110000000");
         $("[data-test-id='agreement']").click();
-        $(".button").click();
-        $(withText("Успешно!")).shouldBe(visible, Duration.ofSeconds(15));
+        $("button.button").click();
+        $(".notification__content")
+                .shouldBe(Condition.visible, Duration.ofSeconds(15))
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + generateDate(dayToAdd, "dd.MM.yyyy")));
     }
 
 }
